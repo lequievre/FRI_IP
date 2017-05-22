c4dedbf2297ce465214677124387e028511687e0
Laurent LEQUIEVRE <laurent.lequievre@univ-bpclermont.fr>
2017-05-22 16:08:48 +0200

    Adding serverip and serverportnumber

--- src/FastResearchInterfaceLibrary/KRCCommunicationThreadMain.cpp
+++ src/FastResearchInterfaceLibrary/KRCCommunicationThreadMain.cpp
@@ -87,8 +87,6 @@ void* FastResearchInterface::KRCCommunicationThreadMain(void *ObjectPointer)
 
 	FRIDataSendToKRC				LocalCommandData;
 
-	FastResearchInterface			*ThisObject						=	(FastResearchInterface*)ObjectPointer;
-
 	memset(ZeroVector, 0x0, NUMBER_OF_JOINTS * sizeof(float));
 	
 #if defined(WIN32) || defined(WIN64) || defined(_WIN64)
